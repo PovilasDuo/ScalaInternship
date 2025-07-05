3e743cb69b40f8ccf211d31648c75f2cd6064a0c
Neda <neda.narmontaite@ktu.edu>
2025-07-05 15:56:47 +0300

    Improved code by combining duplicate logic into more generic methods in the Region class

--- src/main/scala/utility/IO.scala
+++ src/main/scala/utility/IO.scala
@@ -4,53 +4,41 @@ import models.{Location, Region, Results}
 import os.Path
 import upickle.default.*
 import utility.Validator.*
-
 import java.nio.file.NoSuchFileException
 import scala.io.Source
 
 object IO {
-  def readLocations(filePath: String): List[Location] = {
+  private def readList[A: Reader](filePath: String, key: A => String): List[A] = {
+    val originalList = read[List[A]](readFile(filePath))
+    val distinctList = originalList.distinctBy(key)
+
+    if (!(originalList sameElements distinctList)) {
+      println("Duplicate regions found. Accepting the first entry")
+    }
+    distinctList
+  }
+
+  private def readFile(filePath: String): String = {
     val correctFilePath: Path = if checkIfFilePathIsRelative(filePath) then makePathsAbsolute(filePath)
     else os.Path(filePath)
 
     if (os.exists(correctFilePath)) {
       val source = Source.fromFile(filePath)
-      val content =
-        try source.mkString
-        catch case _: Throwable => throw new Exception("Failed processing the file")
-        finally source.close()
-
-      val originalList = read[List[Location]](content)
-      val distinctList = originalList.distinctBy(_.name)
-
-      if (!(originalList sameElements distinctList)) {
-        println("Duplicate locations found. Accepting the first entry")
-      }
-      distinctList
-    }
-    else {
-      throw new NoSuchFileException(filePath)
-    }
-  }
-
-  def readRegions(filePath: String): List[Region] = {
-    if (os.exists(os.Path(filePath))) {
-      val source = Source.fromFile(filePath)
-      val content =
-        try source.mkString
-        catch case _: Throwable => throw new Exception("Failed processing the file")
-        finally source.close()
-
-      val originalList = read[List[Region]](content)
-      val distinctList = originalList.distinctBy(_.name)
-      if (!(originalList sameElements distinctList)) {
-        println("Duplicate regions found. Accepting the first entry")
-      }
-      distinctList
+      try source.mkString
+      catch case _: Throwable => throw new Exception("Failed processing the file")
+      finally source.close()
     }
     else {
       throw new NoSuchFileException("Region file was not found")
     }
+  }
+
+  def readLocations(filePath: String): List[Location] = {
+    readList[Location](filePath, _.name)
+  }
+
+  def readRegions(filePath: String): List[Region] = {
+    readList[Region](filePath, _.name)
   }
 
   def writeResultsFile(results: List[Results], outputFile: String): Unit = {
